fb5853be6f6d92d1cd5b5c17759df12813af1568
tuyennh <tuyen.nh@deha-soft.com>
2021-04-18 10:59:10 +0700

    edit code

--- resources/views/admin/discount_code/index.blade.php
+++ resources/views/admin/discount_code/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <!-- Content Header (Page header) -->
     <section class="content-header">
-        <h1>Quản lý danh mục sản phẩm</h1>
+        <h1>Quản lý mã giảm giá </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
             <li><a href="{{  route('admin.discount.code.index') }}"> Discount Code</a></li>
